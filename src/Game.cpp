d521f22101fd02c9079396b844a8b0e673161c9e
Kruzenger <Kruzenger@gmail.com>
2025-07-27 09:29:59 +0300

    managed constants

--- src/Game.cpp
+++ src/Game.cpp
@@ -22,11 +22,29 @@
 //  is_mouse_button_pressed(int button) - check if mouse button is pressed (0 -
 //  left button, 1 - right button) schedule_quit_game() - quit game after act()
 
+const int64_t kColumnDistance = 1000;
+const int64_t kColumnWidth = 200;
+const int64_t kColumnGapSize = 300;
+const Vector2D kPlayerStartPosition = Vector2D(400, SCREEN_HEIGHT / 2);
+const Vector2D kPlayerSize = Vector2D(50, 50);
+
+// colors
+enum EColors {
+    red = 0x00FF0000,
+    green = 0x0000FF00,
+    blue = 0x000000FF,
+    white = 0x00FFFFFF
+};
+
+const int32_t kPlayerColor = EColors::red;
+const int32_t kColumnColor = EColors::green;
+const int32_t kBackgroundColor = EColors::white;
+
 GameObjectManager GAME_OBJ_MNG;
 std::shared_ptr<GameObject> player;
 std::shared_ptr<FlappyBirdPlayer> flappyBirdPlayer;
 Vector2D camera_position;
-ColumnFactory factrory = ColumnFactory(100, 200, 400, 0x0000FF00);
+ColumnFactory factrory = ColumnFactory(kColumnWidth, kColumnGapSize, 400, kColumnColor);
 int64_t score = 0;
 int64_t distanceCounter = -1;
 
@@ -35,11 +53,11 @@ void initialize()
 {
     camera_position = Vector2D(0, 0);
     player = std::make_shared<GameObject>();
-    player->addComponent(std::make_shared<Rectangle>(50, 50, 0x00FF0000));
-    player->addComponent(std::make_shared<BoxCollider>(50, 50));
+    player->addComponent(std::make_shared<Rectangle>(kPlayerSize.x, kPlayerSize.y, kPlayerColor));
+    player->addComponent(std::make_shared<BoxCollider>(kPlayerSize.x, kPlayerSize.y));
     player->addComponent(std::make_shared<Rigidbody2D>(true));
     player->addComponent(flappyBirdPlayer = std::make_shared<FlappyBirdPlayer>());
-    player->setLocalPosition(400, SCREEN_HEIGHT / 2);
+    player->setLocalPosition(kPlayerStartPosition);
     GAME_OBJ_MNG.addObject(player);
 }
 
@@ -60,8 +78,8 @@ void act(float dt)
     score = (camera_position.x / 100);
     if(score / 10 > distanceCounter) {
         ++distanceCounter;
-        auto obj = factrory.create(300, 150 + (50 * (std::rand() % 6)));
-        obj->setLocalPosition(Vector2D(1000 * (score / 10) + 1100, 0));
+        auto obj = factrory.create(kColumnGapSize, 150 + (50 * (std::rand() % 6)));
+        obj->setLocalPosition(Vector2D(kColumnDistance * (score / 10) + kColumnDistance + 100, 0));
         GAME_OBJ_MNG.addObject(obj);
     }
 
@@ -76,10 +94,10 @@ void draw()
     memset(buffer, 0, SCREEN_HEIGHT * SCREEN_WIDTH * sizeof(uint32_t));
 
     // background // TODO сделать отдельного наследника класса Drawable который будет
-    // рисовать объекты по своим локальным координатам.
+    // рисовать объекты по своим локальным координатам. Сделать фон им.
     for (int i = 0; i < SCREEN_HEIGHT; ++i) {
         for (int j = 0; j < SCREEN_WIDTH; ++j) {
-            buffer[i][j] = 0x00FFFFFF;
+            buffer[i][j] = kBackgroundColor;
         }
     }
 
